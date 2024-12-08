8878ff862791e057bf4aaee15b948f797154761f
haris461 <harisfarooq402@gmail.com>
2024-12-09 04:54:24 +0500

    upload

--- FrontEnd/vite-project/src/App.jsx
+++ FrontEnd/vite-project/src/App.jsx
@@ -6,7 +6,6 @@ import Login from './Components/Jobseeker/Login';
 import RecruiterAndJobSeeker from './Components/Jobseeker/RecruiterAndJobSeeker';
 import SignUp from './Components/Jobseeker/SignUp';
 import Jobsearch from './Components/Jobseeker/Jobsearch';
-// Make sure you import SignUp
 import './App.css';
 
 function App() {
@@ -14,14 +13,12 @@ function App() {
 
   return (
     <Router>
-      
+      <SeekerNavbar />
       <Routes>
-    
         <Route path="/" element={<RecruiterAndJobSeeker />} />
         <Route path="/login/:userType" element={<Login />} />
         <Route path="/signup/:userType" element={<SignUp />} />
-       <Route path="/Jobsearch/:userType" element={<Jobsearch />}/>
-       <SeekerNavbar/>
+        <Route path="/jobsearch/:userType" element={<Jobsearch />} />
       </Routes>
     </Router>
   );
