5c4d04662fc036cce03a318d1a4318c5794dceba
Saiqa Tahir <145122242+saiqa-tahir@users.noreply.github.com>
2025-09-04 19:38:02 +0500

    Delete WageHaus/FYP-WageHaus/Backend directory

--- WageHaus/FYP-WageHaus/Backend/routes/applicationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const applicationController = require('../controllers/applicationController');
-
-router.post('/apply', applicationController.applyForJob);
-router.get('/:recruiterEmail', applicationController.getApplicationsByRecruiter);
-
-module.exports = router;
--- WageHaus/FYP-WageHaus/Backend/routes/predictionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const PredictionController = require("../controllers/PredictionController");
-
-const router = express.Router();
-
-// Next-Word Prediction Route
-router.post("/predict-next-word", PredictionController.predictNextWord);
-
-module.exports = router;
